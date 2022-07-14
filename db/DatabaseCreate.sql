1e0438faf8eea343ad182677dc28e3e0ae1953d8
JakubW99 <92157165+JakubW99@users.noreply.github.com>
2022-07-14 12:56:02 +0200

    adding database creating file

---
+++ db/DatabaseCreate.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE HexGAME
+GO
+CREATE TABLE USER
+(
+UserID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+Email nvarchar(30) UNIQUE NOT NULL,
+_Password nvarchar(30) NOT NULL,
+NickName nvarchar(20) UNIQUE NOT NULL,
+Avatar varchar(400) NOT NULL default 'avatar.jpg'
+);
+GO
