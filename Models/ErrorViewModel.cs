c0b85ab9a54d88cdb2c682f459eba3805d2a427d
qunikarp <jakub.karp01@gmail.com>
2022-07-20 22:24:33 +0200

    Repairing

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HexGame.AspNetCoreApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
